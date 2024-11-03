3d9832a6c386a74ed749f3914fdea98286d50c6c
zhanik228 <zhan142004@gmail.com>
2024-11-03 23:59:43 +0500

    second

--- lib/presentation/chat/chat.dart
+++ lib/presentation/chat/chat.dart
@@ -13,7 +13,7 @@ class Chat extends StatefulWidget {
 
 class _ChatState extends State<Chat> {
   TextEditingController message = TextEditingController();
-  
+  late Future<Map<String, dynamic>> history;
 
   void sendMessage() async {
     var uri = Uri.https('test.mektep-zhmath.kz', '/chat/message');
@@ -23,20 +23,37 @@ class _ChatState extends State<Chat> {
       'message': message.text.toString(),
     });
     var res = await http.post(uri,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-     body: body);
+        headers: {'Content-Type': 'application/json'}, body: body);
     var resJson = jsonDecode(res.body);
     print(resJson);
+    history = getHistory(widget.createdChat['thread']['id']);
+    setState(() {});
+    var historyData = await history;
+    print(historyData['data']);
+  }
+
+  Future<Map<String, dynamic>> getHistory(threadId) async {
+    var uri = Uri.https('test.mektep-zhmath.kz',
+        '/chat/allMessage/${widget.createdChat['thread']['id']}');
+    print(widget.createdChat['thread']['id']);
+    var body = jsonEncode({
+      'thread_ids': widget.createdChat['thread']['id'].toString(),
+      'message': message.text.toString(),
+    });
+    var res =
+        await http.get(uri, headers: {'Content-Type': 'application/json'});
+    var resJson = jsonDecode(res.body);
+    return resJson;
   }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    history = getHistory(widget.createdChat['thread']['id']);
     print(widget.createdChat);
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,11 +64,41 @@ class _ChatState extends State<Chat> {
       body: Column(
         children: [
           Container(
-            padding: EdgeInsets.symmetric(
-              vertical: 20,
-              horizontal: 16
+            height: 500,
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+            child: Expanded(
+              child: FutureBuilder(
+                future: history,
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Center(
+                      child: Text('Error: ${snapshot.error}'),
+                    );
+                  }
+
+                  if (snapshot.hasData) {
+                    print(snapshot.data);
+                    return ListView.builder(
+                      itemCount: snapshot.data?['data'].length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(
+                            snapshot.data?['data'][index]['value'],
+                            style: TextStyle(
+                                color: snapshot.data?['data'][index]['role'] ==
+                                        'user'
+                                    ? Colors.green
+                                    : Colors.white),
+                          ),
+                        );
+                      },
+                    );
+                  }
+
+                  return CircularProgressIndicator();
+                },
+              ),
             ),
-            child: Text('Text'),
           ),
         ],
       ),
@@ -68,14 +115,11 @@ class _ChatState extends State<Chat> {
                   },
                   controller: message,
                   decoration: InputDecoration(
-                    border: InputBorder.none,
-                    filled: true,
-                    fillColor: Color(0xff989FA3).withOpacity(0.08),
-                    labelText: 'Введите текст...',
-                    labelStyle: TextStyle(
-                      color: Color(0xff989FA3)
-                    )
-                  ),
+                      border: InputBorder.none,
+                      filled: true,
+                      fillColor: Color(0xff989FA3).withOpacity(0.08),
+                      labelText: 'Введите текст...',
+                      labelStyle: TextStyle(color: Color(0xff989FA3))),
                 ),
               )
             ],
